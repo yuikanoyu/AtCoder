4b6ea7ecd44a57fe0b37d6910e3d1581b388c3b5
hakone <24824847+yuikanoyu@users.noreply.github.com>
2019-04-01 23:36:14 +0900

    ABC081B実施

--- BeginnersSelection/ABC081B-Shiftonly.py
+++ BeginnersSelection/ABC081B-Shiftonly.py
@@ -1,2 +1,20 @@
-count = int(input())
+def divided(num):
+    return num / 2
+
+def can_divided(num):
+    return num % 2 == 0
+
+_n = int(input())
+num_list = list(map(int, input().split()))
+run_count = 0
+end_flag = False
+
+can_list = list(map(can_divided, num_list))
+while not False in can_list:
+    run_count += 1
+    num_list = list(map(divided, num_list))
+    can_list = list(map(can_divided, num_list))
+    # print(num_list)
+
+print(run_count)
 
