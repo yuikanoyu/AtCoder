afbc33fd6cd23c949e9685b6b6aa3fac966de608
hakone <24824847+yuikanoyu@users.noreply.github.com>
2019-04-02 23:04:07 +0900

    検査用コメントの削除

--- BeginnersSelection/ABC085B-KagamiMochi.py
+++ BeginnersSelection/ABC085B-KagamiMochi.py
@@ -2,7 +2,5 @@ mochi_list = []
 n = int(input())
 for i in range(n):
     mochi_list.append(input())
-    print('i{}'.format(i))
 
-unique_mochi_count=len(list((set(mochi_list))))
-print(unique_mochi_count)
+print(len(list((set(mochi_list)))))
