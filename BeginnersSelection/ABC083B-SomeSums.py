6a37ec04b6c7e8b785f1f876022233a39f895979
hakone <24824847+yuikanoyu@users.noreply.github.com>
2019-04-02 21:55:04 +0900

    strを使う方法の追記

--- BeginnersSelection/ABC083B-SomeSums.py
+++ BeginnersSelection/ABC083B-SomeSums.py
@@ -5,13 +5,21 @@ def digit_sum(num):
         num //= 10
     return total_num
 
+def digit_sum2(num):
+    total = 0
+    for digit in str(num):
+        print('num:{},digit:{}'.format(num,digit))
+        total += int(digit)
+    return total
+
 num_list= []
 n, a, b = map(int, input().split())
 
 for i in range(1,n+1):
-    num = digit_sum(i)
+    num = digit_sum2(i)
     if num >= a and num <= b:
         num_list.append(i)
 
 print(sum(num_list))
+
 
