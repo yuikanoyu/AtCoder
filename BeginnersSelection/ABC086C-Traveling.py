1a5b4de0fdba8db6f3918b8e743a1010321fe183
hakone <24824847+yuikanoyu@users.noreply.github.com>
2019-04-03 23:50:21 +0900

    不要コメントの削除

--- BeginnersSelection/ABC086C-Traveling.py
+++ BeginnersSelection/ABC086C-Traveling.py
@@ -11,16 +11,8 @@ for plan in plans:
     for i in range(plan[0]-end_time):
         if abs(pos_x - plan[1]) > abs(pos_y - plan[2]):
             pos_x += 1 if pos_x <= plan[1] else -1
-#			if pos_x < plan[1]:
-#				pos_x += 1
-#			else:
-#				pos_x -= 1
         else:
             pos_y += 1 if pos_y <= plan[2] else -1
-#			if pos_y < plan[2]:
-#				pos_y += 1
-#			else:
-#				pos_y -= 1
     end_time = plan[0]
     if pos_x != plan[1] or pos_y != plan[2]:
         result = 'No'
