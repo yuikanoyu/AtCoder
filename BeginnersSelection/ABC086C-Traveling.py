4f301daacb3da1fd5b49de9885e1e806a157a5ee
hakone <24824847+yuikanoyu@users.noreply.github.com>
2019-04-03 23:49:42 +0900

    経過時間の減算箇所を修正

--- BeginnersSelection/ABC086C-Traveling.py
+++ BeginnersSelection/ABC086C-Traveling.py
@@ -1,16 +1,15 @@
 pos_x = pos_y = 0
 n = int(input())
 plans = []
-time = 0
+end_time = 0
 result = 'Yes'
 
 for i in range(n):
 	plans.append(list(map(int, input().split(' '))))
 
 for plan in plans:
-	time = plan[0] - time
-	for i in range(time):
-		if abs(pos_x - plan[1]) > abs(pos_y - plan[2]):
+    for i in range(plan[0]-end_time):
+        if abs(pos_x - plan[1]) > abs(pos_y - plan[2]):
             pos_x += 1 if pos_x <= plan[1] else -1
 #			if pos_x < plan[1]:
 #				pos_x += 1
@@ -22,8 +21,8 @@ for plan in plans:
 #				pos_y += 1
 #			else:
 #				pos_y -= 1
-		#print('x:{}, y:{}'.format(pos_x,pos_y))
-	if pos_x != plan[1] or pos_y != plan[2]:
-		result = 'No'
-		break
+    end_time = plan[0]
+    if pos_x != plan[1] or pos_y != plan[2]:
+        result = 'No'
+        break
 print(result)
