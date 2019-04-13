178a40ec3da8e46ecc17cebf59260db9d4101eb8
hakone <24824847+yuikanoyu@users.noreply.github.com>
2019-04-14 03:13:21 +0900

    D完了

--- ABC124/D-ABC124.py
+++ ABC124/D-ABC124.py
@@ -1,16 +1,40 @@
 n,k = map(int,input().split())
 s = tuple(input())
-tmp = 0
-most_tmp = 0
+
+now = 1 # 今見ている数
+cnt = 0 # nowがいくつ並んでいるか
+nums =[]
 
 for i in range(n):
-    if s[i] == '0':
-        tmp += 1
+    if s[i] == str(now):
+        cnt += 1
     else:
-        tmp = 0
+        nums.append(cnt)
+        now ^= 1    #見る値の切り替え
+        cnt = 1
 
-    if most_tmp < tmp:
-        most_tmp = tmp
-    print('{}-{}'.format(most_tmp,tmp))
+if cnt != 0:
+    nums.append(cnt)
 
-print(most_tmp)
+if len(nums) % 2 == 0:
+    nums.append(0)
+
+add = 2 * k + 1
+ans = 0
+left = 0
+right = 0
+tmp = 0
+
+for i in range(len(nums))[::2]:
+    next_left = i
+    next_right = min(i+add, len(nums))
+    # 尺取法
+    while next_left > left:
+        tmp -= nums[left]
+        left += 1
+    while next_right > right:
+        tmp += nums[right]
+        right += 1
+    ans = max(tmp,ans)
+
+print(ans)
