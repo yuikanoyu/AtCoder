1ad6260cda5395e8eba099f54462da417272dcfc
hakone <24824847+yuikanoyu@users.noreply.github.com>
2019-04-16 08:33:40 +0900

    右隣が Cであるような Aを数える方式

--- ABC122/C-ABC122.py
+++ ABC122/C-ABC122.py
@@ -1,5 +1,16 @@
+'''
 n,q = map(int,input().split())
 s = input()
 for _ in range(q):
     l,r = map(int,input().split())
     print(s[l-1:r].count('AC'))
+'''
+n,q = map(int,input().split())
+s = input()
+t = [0]*(n+1)
+for i in range(n):
+    t[i+1] = t[i] + (1 if s[i:i+2] == 'AC' else 0)
+# print(t)
+for i in range(q):
+    l,r = map(int,input().split())
+    print(t[r-1]-t[l-1])
