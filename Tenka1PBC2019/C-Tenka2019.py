2f1269d2ea59e7f760cb19eebc2caba584438a64
hakone <24824847+yuikanoyu@users.noreply.github.com>
2019-04-21 21:17:37 +0900

    実施

--- Tenka1PBC2019/C-Tenka2019.py
+++ Tenka1PBC2019/C-Tenka2019.py
@@ -1,45 +1,10 @@
 N = int(input())
-S = list(input())
+S = input()
+dot_count = S.count('.')
+ans = dot_count
 
-sharp_num =[]
-dot_num = []
+for i in range(N):
+    dot_count += 1 if S[i] == '#' else -1
+    ans = min(dot_count,ans)
 
-init_search = S[0]
-search = init_search
-
-count = 0
-
-for c in S:
-    print('c:{},se:{}'.format(c,search))
-    if search == '#':
-        if c == search:
-            count += 1
-        else:
-            sharp_num.append(count)
-            count = 1
-            search = '.'
-    else:
-        if c == search:
-            count += 1
-        else:
-            dot_num.append(count)
-            count = 1
-            search = '#'
-if init_search == '#':
-    sharp_num.append(count)
-else:
-    dot_num.append(count)
-
-if len(sharp_num) > len(dot_num):
-    dot_num.append(0)
-elif len(sharp_num) < len(dot_num):
-    sharp_num.append(0)
-else:
-    pass
-
-
-total = 0
-for i in range(len(sharp_num)):
-    total += min(sharp_num[i],dot_num[i])
-
-print(total)
+print(ans)
