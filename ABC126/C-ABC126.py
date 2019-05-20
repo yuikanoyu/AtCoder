f05cb22992e15967f81cc28aad095bccd663a4aa
hakone <24824847+yuikanoyu@users.noreply.github.com>
2019-05-20 23:16:09 +0900

    C-ABC126 解き直し

--- ABC126/C-ABC126.py
+++ ABC126/C-ABC126.py
@@ -1,25 +1,12 @@
 N , K = map(int,input().split())
+x = 0
 
+for i in range(1,N+1):
+    y = 0
+    while(i < K):
+        i *= 2
+        y += 1
+    x += (1/2) ** y
 
-times = 0
-total = 1
-while total < K:
-    total *= 2
-    times += 1
-
-bo = N * (2 ** times)    
-
-
-#num = 2**(times-1)-1
-num = 0
-num2 = 1
-for i in range(times-1):
-    num += num2
-    num2 *= 2
-
-#print(times)
-#print(num)
-#print(bo)
-
-print(num/bo)
+print((1/N) * x)
 
