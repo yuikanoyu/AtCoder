3ebb391df793b93754ee5c30455377b951db0bfa
hakone <24824847+yuikanoyu@users.noreply.github.com>
2019-06-09 22:55:28 +0900

    VS動作確認

---
+++ Atcoder_VS/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Atcoder_VS
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            string[] input = Console.ReadLine().Split(' ');
+            int b = int.Parse(input[0]);
+            int c = int.Parse(input[1]);
+            string s = Console.ReadLine();
+            Console.WriteLine((a + b + c) + ' ' + s);
+            Console.WriteLine((a + b + c) + " " + s);
+        }
+    }
+}
