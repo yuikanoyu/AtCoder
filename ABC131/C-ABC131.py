18c73d6399eeeb21189479251597b9cc170552a7
hakone <24824847+yuikanoyu@users.noreply.github.com>
2019-06-22 23:16:01 +0900

    解き直し

--- ABC131/C-ABC131.py
+++ ABC131/C-ABC131.py
@@ -1,19 +1,24 @@
+# only Python 3.4
+import fractions
+
 A, B, C, D = map(int, input().split())
 
 A -= 1
 
+Z = ((C * D) // fractions.gcd(C, D))
+
 A_C = A // C
 A_D = A // D
-A_Z = A // (C * D)
+A_Z = A // Z
 
-A_num = A -( A_C + A_D - A_Z )
+A_num = A - (A_C + A_D - A_Z)
 
 B_C = B // C
 B_D = B // D
-B_Z = B // (C * D)
+B_Z = B // Z
 
 
-B_num = B - ( B_C + B_D - B_Z )
+B_num = B - (B_C + B_D - B_Z)
 
 
 print(B_num - A_num)
