3ca124ea01141f05d6d7fa218f3084a5bed4f078
hakone <24824847+yuikanoyu@users.noreply.github.com>
2019-06-22 14:33:45 +0900

    改造TLE

--- ABC130/D-ABC130.py
+++ ABC130/D-ABC130.py
@@ -8,12 +8,14 @@ for i in range(N):
     x = a[i]
 
     if x >= K:
-        cnt += 1
+        cnt += N-i
+        continue
 
     for j in range(N-i-1):
         x += a[1+i+j]
         if x >= K:
-            cnt += 1
+            cnt += N-(1+i+j)
+            break
 
     
 print(cnt)
