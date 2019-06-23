13a534f55794b741111d50779324a10a3af4cb28
hakone <24824847+yuikanoyu@users.noreply.github.com>
2019-06-23 14:46:36 +0900

    動作確認

---
+++ AtCoderVS/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AtCoderVS
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            string[] input_line = Console.ReadLine().Split();
+            int a = int.Parse(input_line[0]);
+            int b = int.Parse(input_line[1]);
+
+            if ((a * b % 2) == 0 ){
+                Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+            }
+        }
+    }
+}
