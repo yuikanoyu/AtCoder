27a69123fd8aad5584f56eef9768f740d1af67f9
hakone <24824847+yuikanoyu@users.noreply.github.com>
2020-02-20 22:30:46 +0900

    EX8

--- APG4b/main.cpp
+++ APG4b/main.cpp
@@ -1,5 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
+
+int main() {
+    int p;
+    int price;
+    string text;
+    cin >> p;
+
+    // パターン1
+    if (p == 1) {
+        cin >> price;
+    }
+
+    // パターン2
+    if (p == 2) {
+        cin >> text >> price;
+        cout << text << "!" << endl;
+    }
+
+    int N;
+    cin >> N;
+
+    cout << price * N << endl;
+}
 #if EX7
 int main() {
     // 変数a,b,cにtrueまたはfalseを代入してAtCoderと出力されるようにする。
