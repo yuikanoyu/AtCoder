7d1c7d4340f34fe96cbcb9c1af677e044a27bc8a
hakone <24824847+yuikanoyu@users.noreply.github.com>
2020-02-20 21:51:31 +0900

    EX4

--- APG4b/main.cpp
+++ APG4b/main.cpp
@@ -2,7 +2,14 @@
 using namespace std;
 
 int main(){
+    // 一年の秒数
+    int seconds = 365 * 24 * 60 * 60;
 
+    // 以下のコメント/* */を消して追記する
+    cout << 1 * seconds << endl;
+    cout << 2 * seconds << endl;
+    cout << 5 * seconds << endl;
+    cout << 10 * seconds << endl;
 }
 #if EX3
 int main(){
