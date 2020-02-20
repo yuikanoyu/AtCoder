707908ff6d25eb79b7c9dac253a9977851b48df7
hakone <24824847+yuikanoyu@users.noreply.github.com>
2020-02-20 21:47:51 +0900

    EX3

--- APG4b/main.cpp
+++ APG4b/main.cpp
@@ -1,7 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int main(){
+
+}
+#if EX3
+int main(){
+    cout << 100*(100+1)/2 << endl;
+}
+#endif
+
+#if EX2
 int main() {
     cout << "いつも" << 2525 << endl;
     cout << "AtCoderくん" << endl;
 }
+#endif
