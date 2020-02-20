d0264d6640ad11346d3693800551ea938b5edb96
hakone <24824847+yuikanoyu@users.noreply.github.com>
2020-02-20 22:15:56 +0900

    EX7

--- APG4b/main.cpp
+++ APG4b/main.cpp
@@ -1,5 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
+#if EX7
+int main() {
+    // 変数a,b,cにtrueまたはfalseを代入してAtCoderと出力されるようにする。
+    bool a = true;// true または false
+    bool b = false;// true または false
+    bool c = true;// true または false
+
+    // ここから先は変更しないこと
+
+    if (a) {
+        cout << "At";
+    }
+    else {
+        cout << "Yo";
+    }
+
+    if (!a && b) {
+        cout << "Bo";
+    }
+    else if (!b || c) {
+        cout << "Co";
+    }
+
+    if (a && b && c) {
+        cout << "foo!";
+    }
+    else if (true && false) {
+        cout << "yeah!";
+    }
+    else if (!a || c) {
+        cout << "der";
+    }
+
+    cout << endl;
+}
+#endif
+#if EX5
 int main(){
     int A, B;
     string op;
@@ -20,7 +57,7 @@ int main(){
         cout << "error" << endl;
     }
 }
-
+#endif
 #if EX5
 int main(){
     int A, B;
